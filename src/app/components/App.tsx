06dc2cb3d24756fb66b64246a757fc2db7062e5f
Fabrizio Guccione <fg1059@nyu.edu>
2023-07-06 16:49:14 -0400

    Adding Participant and Removing Finished

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,27 +1,57 @@
 import React from 'react'
-
 import { InteractionModeType, Timeline } from '../../timeline'
 
-import dataSet from '../data/smallDataset.json'
+// ------------------------------ DATASET
+//import dataSet from '../data/smallDataset.json'
+import dataSet from '../data/blankDataset.json'
 
-// ------------------------------ MINE
+// ------------------------------ ADDED
 import Button from 'react-bootstrap/Button'
 import {useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// ------------------------------ CODE STARTS
+
 export const App = () => {
+  // React state change of lanes displayed
   const [lanes, setLanes] = React.useState(dataSet.lanes)
 
+
+  function handleBinInput() {
+
+  }
+
+  // function to add new lane displayed 
   function handleAddLane() {
+    // What is the last lane added, or if no lane returns undefined
     const currentLaneNo = lanes.at(lanes.length-1);
-    var laneId = new String(currentLaneNo?.laneId);
-    var laneIdNo = new String(parseInt(laneId.slice(laneId.length-2, )) + 1);
-    const newLane: {laneId: string; label: string;} = {laneId:'lane-0'+(laneIdNo), label:'lane-0'+(laneIdNo)};
 
-    const newListLanes = lanes.concat(newLane);
+    // if undefined returned by currentLaneNo then just add a default first lane
+    if (currentLaneNo == undefined) {
+      const newLane: {laneId: string; label: string;} = {laneId:'Participant-0', label:"Participant-0"};
+      setLanes(lanes.concat(newLane));
+    }
+    // if there is already a lane present and displayed
+    else{
+      // get last lane informations
+      const lane = lanes.at(lanes.length-1);
+      
+      // convert just the lane Id (not what is displayed to the user but what the system reads to recognize it) to a String object
+      const laneId = new String(lane.laneId);
 
-    setLanes(newListLanes);
-    
+      // slice string from the dash (get the number of the lane), and adds one for the new lane to be added 
+      const newLaneIdNo = parseInt(laneId.slice(laneId.indexOf('-')+1, )) + 1;
+     
+      // convert new lane No. to a String so it can be concatenated to the rest of the label
+      const newLaneIdNoS = new String(newLaneIdNo);
+      const newLane: {laneId: string; label: string;} = {laneId:'Participant-'+(newLaneIdNoS), label:'Participant-'+(newLaneIdNoS)};
+
+      // concatenate the new lane to the array of other lanes
+      const newListLanes = lanes.concat(newLane);
+
+      // push the new lane using React set state
+      setLanes(newListLanes);
+    }
   };
 
   function handleRemoveLane() {
@@ -35,13 +65,15 @@ export const App = () => {
     <div>
       <h1>Timeline</h1>
       <div> 
-        <Button type='button' onClick={handleAddLane} variant="outline-primary">Add Participant</Button>
-        <Button type='button' onClick={handleRemoveLane} variant="outline-primary">Remove Participant</Button>
+        <div>
+          <Button type='button' onClick={handleAddLane} variant="outline-primary">Add Participant</Button>
+          <Button type='button' onClick={handleRemoveLane} variant="outline-primary">Remove Participant</Button>
+        </div>
         <Timeline
           events={dataSet.events}
           lanes={lanes}
           dateFormat={(date) => `${date}`}
-          width={1000}
+          width={1400}
           height={300}
           enabledInteractions={[
             InteractionModeType.Hover,
